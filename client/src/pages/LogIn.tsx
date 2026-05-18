55e3da088cc222d19f05874aab32d8730dffbabc
rgsoidev <rgsoi.qns@gmail.com>
2026-05-18 15:15:52 +0800

    change login name

--- client/src/pages/LogIn.tsx
+++ client/src/pages/LogIn.tsx
@@ -78,7 +78,7 @@ export default function Login() {
         {/* Header */}
         <div className="text-center mb-8">
           <h1 className="text-3xl font-bold text-gray-800">
-            QueueIT
+            Queue
           </h1>
 
           <p className="text-gray-500 mt-2">
